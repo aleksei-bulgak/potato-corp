c78b05dbd03f5fef1618f548e9904b47061b9c50
Aleksei Bulgak <aleksei.bulgak@gmail.com>
2017-09-25 09:37:03 +0300

    Update variables.tf

--- users/variables.tf
+++ users/variables.tf
@@ -12,3 +12,20 @@ variable "admin_teams" {
   type    = "string"
   default = "admins"
 }
+
+variable "github_username" {
+  type        = "string"
+  description = "Valid github user name"
+}
+
+variable "user_role" {
+  type        = "string"
+  default     = "member" 
+  description = "User role in organisation. Must be one of member or admin. "
+}
+
+variable "user_team_role" {
+  type        = "string"
+  default     = "member" 
+  description = "User role in team. Must be one of member or maintainer."
+}
